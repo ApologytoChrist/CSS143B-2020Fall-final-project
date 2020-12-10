fb26d625e6e27e8a38a3267a8bb5795aada34a7b
Kengli Fu <kenglif12@gmail.com>
2020-12-09 16:45:25 -0800

    Indexer kinda done Needs work on fringe cases

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,7 +2,7 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +11,38 @@ import java.util.Map;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+        int listSize = docs.size();
+        for (int i = 0; i < docs.size();i++) {
+            String[] docWords = docs.get(i).trim().split("\\s+");
+            Map<String, List<Integer>> wordLocations = wordLocations(docWords);
+            for (String key : wordLocations.keySet()) {
+                if (!indexes.containsKey(key)) {
+                    List<List<Integer>> docsAndLocs = new ArrayList<>(listSize);
+                    for (int fill = 0; fill < listSize; fill++) {
+                        docsAndLocs.add(new ArrayList<>());
+                    }
+                    docsAndLocs.set(i, wordLocations.get(key));
+                    indexes.put(key,docsAndLocs);
+                } else {
+                    indexes.get(key).set(i , wordLocations.get(key));
+                }
+            }
+        }
         return indexes;
+    }
+
+    private Map<String, List<Integer>> wordLocations(String[] words) {
+        Map<String, List<Integer>> wordLocations = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            String currentWord = words[i];
+            if (!wordLocations.containsKey(currentWord)) {
+                List<Integer> wordLocation = new ArrayList<>();
+                wordLocation.add(i);
+                wordLocations.put(currentWord, wordLocation);
+            } else {
+                wordLocations.get(currentWord).add(i);
+            }
+        }
+        return wordLocations;
     }
 }
